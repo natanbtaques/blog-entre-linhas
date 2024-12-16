b6e77de4a5004df06f1e199ee4882c7519f3c5f1
Natan Taques <natanbtaques@gmail.com>
2024-12-16 10:04:57 -0400

    layout changes p.1

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -3,7 +3,6 @@ import { fetchPosts } from "../api";
 import { Post } from "../../types/post";
 import { deletePost } from "../api/deletePost";
 
-
 interface PostDetailProps {
     post: Post;
 }
@@ -13,17 +12,20 @@ export default function PostDetail({ post }: PostDetailProps) {
         <div
             className="min-h-screen flex flex-col items-center bg-[url('/images/image3.jpg')] bg-cover bg-center bg-fixed"
         >
-            <div className="min-h-screen flex flex-col items-center  py-12">
-                <div className="container mx-auto px-6 sm:px-12 md:px-24 bg-white rounded-lg shadow-lg max-w-3xl">
+            <div className="min-h-screen flex flex-col items-center py-12">
+                <div className="container mx-auto px-4 sm:px-6 md:px-12 lg:px-24 bg-white rounded-lg shadow-lg max-w-sm sm:max-w-md md:max-w-lg lg:max-w-3xl">
                     {/* Título do Post */}
-                    <h1 className="text-4xl font-extrabold text-center text-gray-800 mb-4 mt-10">{post.title}</h1>
+                    <h1 className="text-2xl sm:text-3xl md:text-4xl font-extrabold text-center text-gray-800 mb-4 mt-6 sm:mt-10">
+                        {post.title}
+                    </h1>
 
                     <hr className="border-t-2 border-gray-300 mb-6" />
 
                     {/* Conteúdo do Post */}
-                    <div className="space-y-4 text-lg text-gray-700 leading-relaxed">
+                    <div className="space-y-4 text-base sm:text-lg text-gray-700 leading-relaxed">
                         <p>{post.body}</p>
                     </div>
+
                     <div className="flex justify-center space-x-8 my-6">
                         <div className="text-center text-sm text-gray-500">
                             <span>{post.views} visualizações</span>
@@ -33,10 +35,10 @@ export default function PostDetail({ post }: PostDetailProps) {
                         </div>
                     </div>
 
+                    {/* Tags */}
                     <div className="text-center mb-6 text-sm text-gray-500">
-
                         {post.tags && post.tags.length > 0 ? (
-                            <div className="flex flex-wrap justify-center space-x-2">
+                            <div className="flex flex-wrap justify-center gap-2">
                                 {post.tags.map((tag: string, index: number) => (
                                     <span key={index} className="text-white color-2 px-3 py-1 rounded-full">
                                         {tag}
@@ -47,11 +49,13 @@ export default function PostDetail({ post }: PostDetailProps) {
                             <p>No tags available</p>
                         )}
                     </div>
-                    <div>{/* Botões de Ação */}
+
+                    {/* Botões de Ação */}
+                    <div>
                         <div className="flex justify-center mt-8 space-x-4 mb-10">
                             <a
                                 href="/posts"
-                                className=" color-1 text-white px-6 py-3 rounded-full text-lg font-semibold"
+                                className="color-1 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-full text-sm sm:text-lg font-semibold"
                             >
                                 Back to posts
                             </a>
@@ -64,18 +68,15 @@ export default function PostDetail({ post }: PostDetailProps) {
                                         alert("Failed to delete the post");
                                     }
                                 }}
-                                className=" color-2 text-white px-6 py-3 rounded-full text-lg font-semibold hover:bg-color-pur-to-red  "
+                                className="color-2 text-white px-4 sm:px-6 py-2 sm:py-3 rounded-full text-sm sm:text-lg font-semibold hover:bg-color-pur-to-red"
                             >
                                 Delete Post
                             </button>
                         </div>
-
-
                     </div>
-                </div >
+                </div>
             </div>
         </div>
-
     );
 }
 
