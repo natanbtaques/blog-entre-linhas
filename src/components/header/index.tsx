137b13b7fe8cf6a2d8083747e7a10182b3bcbd7c
Natan Taques <natanbtaques@gmail.com>
2024-12-16 11:05:44 -0400

    last changes on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,16 @@
 import 'tailwindcss/tailwind.css';
 import HeaderButton from './headerButton';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <header className="color-2 text-white p-6 shadow-lg sticky top-0">
             <div className="container mx-auto flex justify-between items-center">
                 {/* Título do Blog */}
-
-                <Image src="/logo.png" alt="Blog Logo" width={80} height={50} />
-
+                <Link href={'/home'}>
+                    <Image src="/logo.png" alt="Blog Logo" width={80} height={50} />
+                </Link>
 
                 {/* Navegação */}
                 <nav>
